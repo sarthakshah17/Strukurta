e17951806cf13bca4e85738f4aa63703e648923f
Rishabh Pandey <133371863+rizzabh@users.noreply.github.com>
2024-08-23 17:27:09 +0545

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const footer = () => {
+const Footer = () => {
   return (
     <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
@@ -33,5 +33,5 @@ const footer = () => {
   )
 }
 
-export default footer
+export default Footer
 
